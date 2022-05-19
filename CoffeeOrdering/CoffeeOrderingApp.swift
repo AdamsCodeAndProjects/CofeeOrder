9b941ed8bf93e78573ebe4a6431776668f4ed63e
Adam Januszewski <Adpjan@att.net>
2022-05-19 16:46:35 -0400

    Initial Commit

---
+++ CoffeeOrdering/CoffeeOrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderingApp.swift
+//  CoffeeOrdering
+//
+//  Created by adam janusewski on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeOrdering/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeOrdering
+//
+//  Created by adam janusewski on 5/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
